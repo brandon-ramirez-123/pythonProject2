8d4e60af1352bed299cbb1c22ea3b5baad03e2c3
dev777-sudo <84637326+dev777-sudo@users.noreply.github.com>
2021-07-03 11:50:23 -0500

    d

--- src/seleccionestudiante/modelo/declarative_base.py
+++ src/seleccionestudiante/modelo/declarative_base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///Users/brandonramirez/PycharmProjects/pythonProject2/src/application.sqlite')
+engine = create_engine('sqlite:///aplicacion.db', echo=True)
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
